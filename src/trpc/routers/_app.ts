82f219e3d9fc46a1f74ff44a19583a8b3b6b7c59
Hardik Sharma <hardik2004s@gmail.com>
2025-07-30 13:02:24 +0530

    05: AI and E2B sandbox setup

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -5,7 +5,7 @@ export const appRouter = createTRPCRouter({
   invoke: baseProcedure
   .input(
     z.object({
-      text: z.string(),
+      value: z.string(),
     })
   )
 
@@ -13,9 +13,10 @@ export const appRouter = createTRPCRouter({
     await inngest.send({
       name: "test/hello.world",
       data: {
-        email: input.text,
+        value: input.value,
       }
     })
+    return { ok: "success" };
   }),
 
 });
